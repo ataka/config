fdd7947925dfba38ff6d053390131ccaec4273be
Masayuki Ataka <masayuki.ataka@gmail.com>
2024-11-10 21:43:34 +0900

    Make symbolic link for Xcode Font and Color Themes with GNU stow

--- link.sh
+++ link.sh
@@ -5,3 +5,6 @@ stow --target ~ screen tmux zsh
 
 # ~/.config に置く設定ファイル
 stow --dotfiles --target ~ git wezterm
+
+# ~/Library/Developer/Xcode に置く設定ファイル
+stow --target ~/Library/Developer/Xcode Xcode
